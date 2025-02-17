3f304bfe3f680974cc178361e985770120a6e15a
Gustavo castro <gustavonascimentocastro1@gmail.com>
2025-02-17 20:14:10 -0300

    project base commit-playground

--- playground.py
+++ playground.py
@@ -7,3 +7,6 @@ path_file = "video/beam.avi"
 Video = Video(path_file) #aqui vamos passar o valor da variavel pro outro arquivo 
 
 vetor_flatten_2d = Video.upload_video_3D_4_2D() #aqui a função so abre o video 
+
+
+"essa variavel 'vetor_flatten_2d' que deve ser passado como parametro das outras funções, chamem todas as outras funções"
